c200dd39b348a6091be175e45f295ca7c4e6e164
mark <mark.vanderloo@gmail.com>
2014-10-22 18:30:58 +0200

    better cloning/merging. Vignette writing

--- R/options.R
+++ R/options.R
@@ -10,7 +10,7 @@ NULL
 
 OPTIONREGISTER <- new.env()
 
-#' Register an option set
+#' Register an options set
 #'
 #' The function \code{register_options} sets up a new options register. It returns
 #' a function that can be uset to set, get, or reset options; or to return a reference to 
@@ -19,7 +19,7 @@ OPTIONREGISTER <- new.env()
 #' The following terms are arguments of the function that is returned and cannot be used as
 #' option names:
 #' 
-#' \code{.where} \code{.simplify} \code{.reset} \code{.clone} \code{.ref}
+#' \code{.where} \code{.reset} \code{.clone} \code{.ref}
 #'
 #'
 #' @param name name of option set
@@ -30,7 +30,6 @@ OPTIONREGISTER <- new.env()
 #' \tabular{ll}{
 #' \code{...} \tab Comma separated list of option names (\code{character}) to retrieve options or \code{[name]=[value]} pairs to set options.\cr
 #' \code{.where} \tab (\code{environment}; \code{NULL}) Where to set options (optional, default is global) \code{environment} that was obtained using the \code{clone} or \code{ref} option. \cr
-#'  \code{.simplify}\tab (\code{logical}; \code{TRUE}) Attempt to simplify list of retrieved options?\cr
 #'  \code{.reset} \tab (\code{logical}; \code{FALSE}) Reset options to default values\cr
 #'  \code{.clone} \tab (\code{logical}; \code{FALSE}) Create a copy of the options (returns newly created environment)\cr
 #'  \code{.ref} \tab  (\code{logical}; \code{FALSE}) Return a reference to the option environment.
@@ -45,9 +44,8 @@ OPTIONREGISTER <- new.env()
 #' my_options('baz')
 #' my_options('foo')
 #' 
-#' # When multiple options are retrieved, the result is simplified unless specified otherwise:
+#' # When multiple options are retrieved, the result is a list
 #' my_options('foo','baz')
-#' my_options('foo','baz',simplify=FALSE)
 #' 
 #' ### Setting global options
 #' my_options(foo=3,baz='pete')
@@ -71,6 +69,7 @@ OPTIONREGISTER <- new.env()
 #' my_options('foo',where=op_ref) #warning: this changes global settings!
 #' my_options('foo')
 #' 
+#' @seealso \code{\link{reset}}, \code{\link{clone_and_merge}}
 #' @export
 register_options <- function(name,defaults=list()){
   reg <- function(n,d){
@@ -78,28 +77,13 @@ register_options <- function(name,defaults=list()){
       warning(sprintf("Overwriting pre-existing option register '%s'",name))
     OPTIONREGISTER[[name]] <- new.env()
     OPTIONREGISTER[[name]]$options <- defaults
+    OPTIONREGISTER[[name]]$defaults <- defaults
     OPTIONREGISTER[[name]]
   }
   op <- reg(name,defaults)
   
-  get_option <- function(where, opts, simplify){
-    if (length(opts) == 0 ) opts <- names(where$options)
-    if (simplify) 
-      sapply(opts, function(x) where$options[[x]]) 
-    else 
-      where$options[unlist(opts)]
-  }
   
-  set_option <- function(where, opts, reset){
-    if (reset){
-      where$options <- defaults
-    } else {
-      where$options[names(opts)] <- opts
-    }
-    where$options
-  }
-  
-  function(..., .where=NULL, .simplify=TRUE, .reset=FALSE, .clone=FALSE, .ref=FALSE){
+  function(..., .where=NULL, .reset=FALSE, .clone=FALSE, .ref=FALSE){
     L <- list(...)
     # global settings if no local setting is pointed to.
     if ( is.null(.where) ) .where <- op
@@ -108,13 +92,68 @@ register_options <- function(name,defaults=list()){
     get <- ( length(L) > 0 && is.null(names(L)) ) ||
            ( length(L) == 0 && !.clone && !.ref )
     if (set)    return( invisible(set_option(.where, L, .reset)) )
-    if (get)    return( get_option(.where, L, .simplify) )
+    if (get)    return( get_option(.where, L) )
     if (.clone)  return( as.environment(as.list(.where)) )
     if (.ref)    return( .where )
     stop('Illegal command')
   }
 }
 
+get_option <- function(where, opts){
+  if (length(opts) == 0 ) opts <- names(where$options)
+  if (length(opts) == 1) 
+    where$options[[unlist(opts)]] 
+  else 
+    where$options[unlist(opts)]
+}
+
+set_option <- function(where, opts, reset){
+  if (reset){
+    where$options <- where$defaults
+  } else {
+    where$options[names(opts)] <- opts
+  }
+  where$options
+}
+
+
+
 clear_optionregister <- function() rm(list=ls(OPTIONREGISTER),envir=OPTIONREGISTER)
+
+#' Manipulate option set
+#'
+#' @param options A function as returned by \code{\link{register_options}}
+#' @param ... Options to be merged. 
+#'
+#' @return A function like \code{options}. However, the \code{.where} argument cannot be specified
+#'   as this function only stores, (re)sets and gets locally options.
+#'   
+#' @seealso \code{\link{register_options}}
+#' @export 
+clone_and_merge <- function(options,...){
+  op <- options(.clone=TRUE)
+  options(..., .where=op)
+  # This explicit construction prevents nested calls when clone_and_merge is called
+  # repeatedly and iteratively on an option structure. 
+  f <- function(...,.where=NULL,.reset=FALSE,.clone=FALSE,.ref=FALSE){}
+  body(f) <- body(options)
+  f
+}
+
+#' Reset options to default values
+#' @rdname clone_and_merge
+#' @export 
+reset <- function(options) options(.reset=TRUE)
+
+#' List of reserved words
+#' 
+#' Returns a list of words that may not be used as option name.
+#' 
+#' @export
+reserved <- function() c(".where",".clone",".ref",".reset")
+
+
+
+
 
 
